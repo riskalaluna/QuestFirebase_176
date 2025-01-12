00308ba8c45527073736d6786c9661aa92923ae6
riskalaluna <riskalaluna@gmail.com>
2025-01-12 14:36:06 +0700

    merepresentasikan status error pada suatu formulir

--- app/src/main/java/com/example/praktikum11_176/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum11_176/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,20 @@ package com.example.praktikum11_176.ui.viewmodel
 
 import com.example.praktikum11_176.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 //data class variabel yang menyampaikan data input form
 data class MahasiswaEvent(
     val nim: String = "",
