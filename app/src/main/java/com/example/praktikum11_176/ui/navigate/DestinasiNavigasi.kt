a740ceb38225a0db7afbf62e6571f5731f2d3254
riskalaluna <riskalaluna@gmail.com>
2025-01-12 19:24:18 +0700

    menambahkan DestinasiHome di DestinasiNavigasi

---
+++ app/src/main/java/com/example/praktikum11_176/ui/navigate/DestinasiNavigasi.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum11_176.ui.navigate
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
