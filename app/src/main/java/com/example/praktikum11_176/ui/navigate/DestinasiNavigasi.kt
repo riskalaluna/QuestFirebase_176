32e02755df310a89ac05792a17db7e9e83cc6d65
riskalaluna <riskalaluna@gmail.com>
2025-01-12 19:27:13 +0700

    menambahkan DestinasiInsert di dalam DestinasiNavigasi

--- app/src/main/java/com/example/praktikum11_176/ui/navigate/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/praktikum11_176/ui/navigate/DestinasiNavigasi.kt
@@ -10,3 +10,7 @@ object DestinasiHome : DestinasiNavigasi {
     override val titleRes: String = "Home"
 }
 
+object DestinasiInsert : DestinasiNavigasi {
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
