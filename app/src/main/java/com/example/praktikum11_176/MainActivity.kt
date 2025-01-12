40f9bc7be290431f586bc9a9fbbc528c45518c76
riskalaluna <riskalaluna@gmail.com>
2025-01-12 20:27:43 +0700

    Mengatur untuk menjalankan code dengan menambahkan PengelolaHalaman

--- app/src/main/java/com/example/praktikum11_176/MainActivity.kt
+++ app/src/main/java/com/example/praktikum11_176/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikum11_176.ui.navigate.PengelolaHalaman
 import com.example.praktikum11_176.ui.theme.Praktikum11_176Theme
 import com.example.praktikum11_176.ui.view.HomeScreen
 
@@ -21,7 +22,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Praktikum11_176Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(navigateToItemEntry = {}, modifier =  Modifier.padding(innerPadding))
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding),
+                    )
                 }
             }
         }
